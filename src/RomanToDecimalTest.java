e41acadee95bc0df0583b3ad08e71051c7ca14fb
Roedere <26roederer@da.org>
2022-10-13 20:41:20 -0400

    changes

--- src/RomanToDecimalTest.java
+++ src/RomanToDecimalTest.java
@@ -8,9 +8,14 @@ public class RomanToDecimalTest {
         assertEquals(RomanToDecimal.romanToDecimal("Hi mom"),-1);
         assertEquals(RomanToDecimal.romanToDecimal("IV"),4);
         assertEquals(RomanToDecimal.romanToDecimal("IX"),9);
-        assertEquals(RomanToDecimal.romanToDecimal("`IVIVIVIV`"),16);
+        assertEquals(RomanToDecimal.romanToDecimal("MC"),1100);
+        assertEquals(RomanToDecimal.romanToDecimal("cx"),110);
 
 
         assertNotEquals(RomanToDecimal.romanToDecimal("yourmom"),100);
+        assertNotEquals(RomanToDecimal.romanToDecimal("Soccer"),50);
+        assertNotEquals(RomanToDecimal.romanToDecimal("UNC"),65);
+        assertNotEquals(RomanToDecimal.romanToDecimal("DA"),95);
+        assertNotEquals(RomanToDecimal.romanToDecimal("baseball"),95);
     }
 }
